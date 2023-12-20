81e719b5de97d0819ca29d02b7b88900a04ab14b
zzliujianbo <zzliujianbo@foxmail.com>
2023-12-20 11:58:58 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use log::{debug, info};
+
+pub async fn run() -> Result<(), std::io::Error> {
+    info!("newoneblog start");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use newoneblog;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    newoneblog::run().await
 }
