e30ee5f4d3b2867850534e844c8ad0cdf6a62c63
zzliujianbo <zzliujianbo@foxmail.com>
2024-01-05 18:01:40 +0800

    增加pinyin

--- src/md.rs
+++ src/md.rs
@@ -1,7 +1,8 @@
-use std::fmt::format;
+use std::{fmt::format, fs::File, path::Path};
 
 use crate::conf::Conf;
-use log::info;
+use log::{error, info, debug};
+use pinyin::{to_pinyin_vec, Pinyin};
 use tera::{self, Context};
 
 /// markdown to html
@@ -33,11 +34,34 @@ pub async fn gen_html(md_files: Vec<String>, conf: &Conf) -> Result<(), tera::Er
     md_files.iter().for_each(|md_file| {
         info!("md_file: {}", md_file);
         let mut context = Context::new();
-        context.insert("content", "");
+        context.insert("content", "123");
         let r = templates.render("content.html", &context);
         match r {
             Ok(s) => {
-                info!("s:{}", s);
+                let md_path = Path::new(md_file);
+                //获取文件路径
+                let file_name = md_path.file_name();
+                let file_name = match file_name {
+                    Some(s) => s.to_str().unwrap(),
+                    None => {
+                        error!("file_name is None");
+                        return;
+                    }
+                };
+                debug!("file_name:{}", file_name);
+                //中文转拼音
+                let pinyin = to_pinyin_vec(file_name, Pinyin::plain);
+                let pinyin = pinyin.join("");
+                debug!("pinyin:{}", pinyin);
+                let mut html = Path::new(&conf.public_path).join(md_path.parent().unwrap());
+                html.set_file_name(format!("{}.html", pinyin));
+                let html_path_str = html.to_str().unwrap();
+                let Ok(f) = File::create(html_path_str) else {
+                    error!("create file error");
+                    return;
+                };
+
+                info!("f:{}", html_path_str);
             }
             Err(e) => {
                 info!("e:{}", e);
