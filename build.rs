32a2225f37af4cb5e5e8e8ba2c4c1d58c30112b9
zzliujianbo <zzliujianbo@foxmail.com>
2024-02-01 14:53:58 +0800

    增加编译脚本，编译时复制资源文件到输出目录。

---
+++ build.rs
@@ -0,0 +1,14 @@
+use copy_to_output::copy_to_output;
+use std::env;
+
+fn main() {
+    //拷贝资源文件到编译目录
+    println!("cargo:rerun-if-changed=templates/*");
+    println!("cargo:rerun-if-changed=conf.json");
+    //let out_dir = env::var_os("OUT_DIR").unwrap();
+    //println!("out_dir: {:?}", out_dir);
+    let profile = env::var("PROFILE").unwrap();
+    println!("build profile: {:?}", profile);
+    copy_to_output("templates", &profile).expect("templates not copy");
+    copy_to_output("conf.json", &profile).expect("conf.json not copy");
+}
